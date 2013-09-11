c121577bfdae1d5301002953bf8c09050a5f008d
Henrik Nygren <nygrenh@gmail.com>
2013-09-11 21:18:14 +0300

    Lisättty pelilautaluokka ja sen testiluokka

---
+++ Laivanupotus/src/objektit/Pelilauta.java
@@ -0,0 +1,21 @@
+package objektit;
+
+public abstract class Pelilauta {
+    private Ruutu[][] ruudut;
+    
+    public Pelilauta(int koko){
+        ruudut = new Ruutu[10][10];
+        for(int i=0; i<ruudut.length; i++){
+            for(int j=0; j<ruudut[0].length; j++){
+                ruudut[i][j] = new Ruutu();
+            }
+        }
+    }
+    
+    public Ruutu getRuutu(int x, int y){
+        if(x>=0 && y>=0 && x<ruudut.length && y<ruudut[0].length){
+            return ruudut[x][y];
+        }
+        return null;
+    }
+}
---
+++ Laivanupotus/test/objektit/PelilautaTest.java
@@ -0,0 +1,30 @@
+
+package objektit;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PelilautaTest {
+    private Pelilauta lauta;
+    
+    @Before
+    public void setUp() {
+        lauta = new Pelilauta(10) {           
+        };
+    }
+
+    @Test
+    public void getRuutuPalauttaaNullJosRuutuaEiOleOlemassa() {
+        assertNull(lauta.getRuutu(10, 10));
+        assertNull(lauta.getRuutu(-1, -1));
+    }
+    
+    @Test
+    public void getRuutuPalauttaaRuudunJosRuutuOnOlemassa(){
+        assertTrue(lauta.getRuutu(0, 0) instanceof Ruutu);
+        assertTrue(lauta.getRuutu(9, 9) instanceof Ruutu);
+    }
+    
+}
+
