268bce6eaf44845a017aa1320b82c905f40cfb83
Henrik Nygren <nygrenh@gmail.com>
2013-09-12 18:41:21 +0300

    Korjattu bugi, jossa pelilaudan koko oli kovakoodattu jostakin käsittämättömästä syystä

--- Laivanupotus/src/objektit/Pelilauta.java
+++ Laivanupotus/src/objektit/Pelilauta.java
@@ -5,8 +5,8 @@ public class Pelilauta {
     private boolean[][] pommitetutruudut; 
     
     public Pelilauta(int koko){
-        ruudut = new Ruutu[10][10];
-        pommitetutruudut = new boolean[ruudut.length][ruudut[0].length];
+        ruudut = new Ruutu[koko][koko];
+        pommitetutruudut = new boolean[koko][koko];
         for(int i=0; i<ruudut.length; i++){
             for(int j=0; j<ruudut[0].length; j++){
                 ruudut[i][j] = new Ruutu();
