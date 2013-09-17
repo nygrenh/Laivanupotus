7ea11fb1fb60feac8c0533a8a4aa2866ee18dd7d
Henrik Nygren <nygrenh@gmail.com>
2013-09-17 09:45:53 +0300

    Lisätty logiikka

---
+++ Laivanupotus/src/logiikka/Logiikka.java
@@ -0,0 +1,14 @@
+package logiikka;
+
+import toimijat.Pelaaja;
+
+public class Logiikka {
+    private Pelaaja pelaaja;
+    
+    public Logiikka(){
+        pelaaja = new Pelaaja();
+    }
+    public void silmukka(){
+        
+    }
+}
