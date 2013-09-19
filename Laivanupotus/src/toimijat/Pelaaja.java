27dee0ea48f4002a6e71f067bdd24cf13fbb8b84
Henrik Nygren <nygrenh@gmail.com>
2013-09-19 18:19:31 +0300

    Pelaajan sisällön alustavaa toteutusta.

--- Laivanupotus/src/toimijat/Pelaaja.java
+++ Laivanupotus/src/toimijat/Pelaaja.java
@@ -1,5 +1,20 @@
 package toimijat;
 
+import java.util.Collection;
+import objektit.Laiva;
+import objektit.Pelilauta;
+
 public class Pelaaja {
+    private Pelilauta pelilauta, vastustajanPelilauta;
+    private Collection<Laiva> laivat;
     
+    public Pelaaja(){
+        this.pelilauta = new Pelilauta(10);
+    }
+    
+    public void sijoitaLaivatLaudalle(Collection<Laiva> laivat){
+        for(Laiva laiva : laivat){
+            
+        }
+    }
 }
