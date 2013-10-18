7460df6c9b2a5f11b587575e365c874fbaac8808
Henrik Nygren <nygrenh@gmail.com>
2013-10-18 16:37:02 +0300

    Lisätty testejä

--- Laivanupotus/test/logiikka/tekoaly/TekoalyTest.java
+++ Laivanupotus/test/logiikka/tekoaly/TekoalyTest.java
@@ -1,6 +1,6 @@
 package logiikka.tekoaly;
 
-import logiikka.tekoaly.Tekoaly;
+import objektit.Laiva;
 import objektit.Pelilauta;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,5 +28,25 @@ public class TekoalyTest {
             }
         }
         fail("Mitään ruutua ei ole pommitettu");
+    }
+
+    /**
+     * Ei ole ihan idioottivarma, mutta ainoa tapa testata
+     * asiaa, koska kaikki muut luokan metodit ovat yksityisiä
+     */
+    @Test
+    public void siirraYrittaaEtsiaLaivaaOsumanJalkeen() {
+        Laiva laiva = new Laiva(2, "Kumivene");
+        pelilauta.asetaLaivaLaudalle(laiva, 1, 1, Boolean.FALSE);
+        while (true) {
+            if (tekoaly.siirra()) {
+                break;
+            }
+        }
+        tekoaly.siirra();
+        Boolean viereistaRuutuaOnPommitettu = pelilauta.ruutuaOnJoPommitettu(0, 1) || pelilauta.ruutuaOnJoPommitettu(1, 0)
+                || pelilauta.ruutuaOnJoPommitettu(2, 1) || pelilauta.ruutuaOnJoPommitettu(2, 2)
+                || pelilauta.ruutuaOnJoPommitettu(1, 3) || pelilauta.ruutuaOnJoPommitettu(0, 2);
+        assertTrue(viereistaRuutuaOnPommitettu);
     }
 }
